be47973e13b83b6ea3dfec0f3f1b1f15fa6c63e2
Anton Laryionau <alaryionau@evolution.com>
2026-05-25 22:16:44 +0200

    cross-compile to scala 3

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,9 +1,13 @@
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.7.2")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.4.4")
 
-addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.2.7")
+addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.15")
 
-addSbtPlugin("com.github.sbt" % "sbt-release" % "1.0.15")
+addSbtPlugin("com.github.sbt" % "sbt-release" % "1.5.0")
 
 addSbtPlugin("com.evolution" % "sbt-scalac-opts-plugin" % "0.0.9")
 
 addSbtPlugin("com.evolution" % "sbt-artifactory-plugin" % "0.0.2")
+
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.6.1")
+
+libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
--- src/test/scala/com/evolutiongaming/util/throttler/RequestThrottlerSpec.scala
+++ src/test/scala/com/evolutiongaming/util/throttler/RequestThrottlerSpec.scala
@@ -1,71 +1,73 @@
 package com.evolutiongaming.util.throttler
 
-import java.util.concurrent.Executors
-
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalatestplus.mockito.MockitoSugar
 import org.scalatest.time.SpanSugar
 
+import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 
-class RequestThrottlerSpec extends AnyFlatSpec with Matchers with MockitoSugar with ScalaFutures with SpanSugar {
+class RequestThrottlerSpec extends AnyFlatSpec with Matchers with ScalaFutures with SpanSugar {
 
   "RequestThrottler" should "allow all requests if disabled" in new Scope(enabled = false) {
-   
+
     val keys = List("session1", "session2", "session3")
 
-    val failedList = for (key <- keys; _ <- 0L to rate * 2; if !(requestThrottler isAllowed key)) yield key
+    val failedList = for {
+      key <- keys
+      _ <- 0L to rate * 2 if !requestThrottler.isAllowed(key)
+    } yield key
 
     failedList shouldBe empty
   }
 
-  it should  "cut requests above allowed fixed rate" in new Scope(period = 10000L) {
-    
+  it should "cut requests above allowed fixed rate" in new Scope(period = 10000L) {
+
     val TestKeys = 10
     val Threads = 10
 
-    implicit val executor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Threads))
+    implicit val executor: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Threads))
 
-    val futures = for {_ <- 1 to TestKeys} yield {
+    val futures = for { _ <- 1 to TestKeys } yield {
       Future {
         val key = System.nanoTime().toString
 
-        for {_ <- 1L to rate} {
-          requestThrottler isAllowed key shouldBe true
+        for { _ <- 1L to rate } {
+          requestThrottler.isAllowed(key) shouldBe true
         }
-        for {_ <- 1L to rate * 2} {
-          requestThrottler isAllowed key shouldBe false
-        }
-
-        Thread sleep period / 2
-
-        for {_ <- 1L to rate * 2} {
-          requestThrottler isAllowed key shouldBe false
+        for { _ <- 1L to rate * 2 } {
+          requestThrottler.isAllowed(key) shouldBe false
         }
 
-        Thread sleep period / 2
+        Thread.sleep(period / 2)
 
-        for {_ <- 1L to rate} {
-          requestThrottler isAllowed key shouldBe true
+        for { _ <- 1L to rate * 2 } {
+          requestThrottler.isAllowed(key) shouldBe false
         }
-        for {_ <- 1L to rate * 2} {
-          requestThrottler isAllowed key shouldBe false
+
+        Thread.sleep(period / 2)
+
+        for { _ <- 1L to rate } {
+          requestThrottler.isAllowed(key) shouldBe true
+        }
+        for { _ <- 1L to rate * 2 } {
+          requestThrottler.isAllowed(key) shouldBe false
         }
       }
     }
 
     // will fail the test in case of failed future(s)
-    implicit val patienceConfig = PatienceConfig(timeout = 30.seconds, interval = 200.millis)
+    implicit val patienceConfig: PatienceConfig = PatienceConfig(timeout = 30.seconds, interval = 200.millis)
     (Future sequence futures).futureValue
   }
-  
+
   private abstract class Scope(val enabled: Boolean = true, val rate: Long = 10L, val period: Long = 1000L) {
     val requestThrottler = RequestThrottler(
       rejectedMeter = () => {},
       enabled = () => enabled,
       allowedRate = () => rate,
-      throttlingPeriodMillis = () => period)
+      throttlingPeriodMillis = () => period,
+    )
   }
 }
